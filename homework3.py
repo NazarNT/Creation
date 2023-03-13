fe101c8bbcfd4c90c8b256581cb28bceb8f9bfec
NazarNT <nazar.skoropad@gmail.com>
2023-03-13 21:46:17 +0200

    Homework 3

---
+++ homework3.py
@@ -0,0 +1,28 @@
+# Homework 3 Part 1 version 1
+print(" I love Python "*42)
+print( )
+# Homework 3 Part 1 version 2
+to_print = "I love Python "
+print (to_print*42)
+print( )
+# Homework 3 Part 2
+age_in_month = 432
+age_in_years = age_in_month/12
+my_name = "Nazar"
+my_age = f"My name is {my_name}. I'm {int(age_in_years)} years old"
+print (my_age)
+print( )
+# Homework 3 Part 3
+first_variable = 1
+print(first_variable == 1)
+print(first_variable != 2)
+print(first_variable > 0)
+print(first_variable < 0)
+print(first_variable >= 2)
+print(first_variable <= 0)
+# Homework 3 Part 4
+a = 2
+b = 5
+c = 6
+d = str(a)+str(b)+str(c)
+print (d)
